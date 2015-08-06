0b9ed8f0a8de685b720b9d1bce6e1f1aa4ff2853
shawnlawyer <shawnlawyer@gmail.com>
2015-08-06 03:10:01 -0400

    moo

--- classes/Packages/Sequode/SQDE_SequodeOriginsRest.class.php
+++ classes/Packages/Sequode/SQDE_SequodeOriginsRest.class.php
@@ -15,8 +15,7 @@ class SQDE_SequodeOriginsRest{
         $sequode_model->getAll($where,'id,name,detail,usage_type,coding_type,sequence,input_object,property_object,output_object,input_object_detail,property_object_detail,output_object_detail,input_object_map,property_object_map,output_object_map,input_form_object,property_form_object');
         
         SQDE_Sequode::exists(SQDE_Package::model()->sequode_id,'id');
-        $package_sequode_model_ids = unique_array(json_decode(SQDE_Sequode::model()->sequence));
-        
+        $package_sequode_model_ids = array_unique(json_decode(SQDE_Sequode::model()->sequence));
         
         $sequode_model->getAll($where,'id,name,detail,usage_type,coding_type,sequence,input_object,property_object,output_object,input_object_detail,property_object_detail,output_object_detail,input_object_map,property_object_map,output_object_map,input_form_object,property_form_object');
         $models = $sequode_model->all;
