6ce1bf1dde6507e74636b68b5d83f9ceb9dee5c9
shawnlawyer <shawnlawyer@gmail.com>
2015-08-14 20:10:15 -0400

    moo

--- includes/js/SQDE_Model.js
+++ includes/js/SQDE_Model.js
@@ -130,8 +130,10 @@ var SQDE_Model = function(){
 		o.inpObj.fill = config.model.button_type_colors[m];
 		o.inpObj.x = config.model.button_positions_x[m];
 		o.inpObj.y = self.height - ( config.model.padding_height + ( i * config.model.segment_height) + config.model.button_type_height_adjustment[m] );
+        console.log(o.inpObj);
 		o.shape = shapesKit.circle(o.inpObj);
-        if( self.default_events == true ){
+        console.log(o.inpObj);
+		if( self.default_events == true ){
             var t = false;
             o.shape.on('mouseout', function(){
                 if(t != false){
@@ -153,7 +155,8 @@ var SQDE_Model = function(){
                         self.parent.wiring_layer.hide();
                     }
                     t.inpObj = config.get('model','tip_label');
-                    cnsole.log(o);
+                    console.log(o.inpObj);
+		
                     t.inpObj.x = o.inpObj.x;
                     t.inpObj.y = o.inpObj.y - o.inpObj.radius;
                     t.shape = shapesKit.label(t.inpObj);
