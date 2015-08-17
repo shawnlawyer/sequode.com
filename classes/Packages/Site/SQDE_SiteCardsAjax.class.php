5a631d55606429bbdc3a37109f8f6f21b6a2cd01
shawnlawyer <shawnlawyer@gmail.com>
2015-08-17 03:28:09 -0400

    moo

--- classes/Packages/Site/SQDE_SiteCardsAjax.class.php
+++ classes/Packages/Site/SQDE_SiteCardsAjax.class.php
@@ -12,29 +12,47 @@ class SQDE_SiteCardsAjax {
         $js[] = $card->js;
         return implode(' ',$js);
     }
+    public static function sequodesMenu(){
+        return SQDE_SequodeCardObjects::menu();
+    }
+    public static function authMenu(){
+        return SQDE_AuthCardObjects::menu();
+    }
+    public static function usersMenu(){
+        return SQDE_UsersCardObjects::menu();
+    }
+    public static function machinesMenu(){
+        return SQDE_MachineCardObjects::menu();
+    }
+    public static function packagesMenu(){
+        return SQDE_PackageCardObjects::menu();
+    }
+    public static function sessionsMenu(){
+        return SQDE_SessionCardObjects::menu();
+    }
     public static function menus($dom_id = 'MenusContainer'){
         $html = $js = array();
-        $card = SQDE_Cards::render(self::$package,'authMenu');
+        $card = SQDE_Cards::render('Auth','menu');
         $html[] = SQDE_Card::menuCardHidingContainer($card->html,9);
         $js[] = $card->js;
         if(SQDE_UserAuthority::isAuthenticated()){
-            $card = SQDE_Cards::render(self::$package,'sequodesMenu');
+            $card = SQDE_Cards::render('Sequode','menu');
             $html[] = SQDE_Card::menuCardHidingContainer($card->html,8);
             $js[] = $card->js;
             if(SQDE_AuthenticatedUser::model()->role_id < 101){
-                $card = SQDE_Cards::render(self::$package,'usersMenu');
+                $card = SQDE_Cards::render('Users','menu');
                 $html[] = SQDE_Card::menuCardHidingContainer($card->html,7);
                 $js[] = $card->js;
-                $card = SQDE_Cards::render(self::$package,'packagesMenu');
+                $card = SQDE_Cards::render('Package','menu');
                 $html[] = SQDE_Card::menuCardHidingContainer($card->html,6);
                 $js[] = $card->js;
-                $card = SQDE_Cards::render(self::$package,'machinesMenu');
+                $card = SQDE_Cards::render('Machine','menu');
                 $html[] = SQDE_Card::menuCardHidingContainer($card->html,5);
                 $js[] = $card->js;
             }
         }
         if(SQDE_UserAuthority::isSystemOwner()){ 
-            $card = SQDE_Cards::render(self::$package,'sessionsMenu');
+            $card = SQDE_Cards::render('Session','menu');
             $html[] = SQDE_Card::menuCardHidingContainer($card->html,4);
             $js[] = $card->js;
         }
