7c7c2b0d1ac37608b2b62e4ee0f1f9b54c97026c
shawnlawyer <shawnlawyer@gmail.com>
2015-08-17 02:08:25 -0400

    moo
    moo

--- classes/Packages/Account/SQDE_AccountCardObjects.class.php
+++ classes/Packages/Account/SQDE_AccountCardObjects.class.php
@@ -1,8 +1,9 @@
 <?php
 class SQDE_AccountCardObjects {
     public static $package = 'Account';
-    public static function details($user_model=null){
-        if($user_model == null ){ $user_model = SQDE_User::model(); }
+    public static $modeler = 'SQDE_User';
+    public static function details($_model = null){
+         $_model = ($_model == null ) ? forward_static_call_array(array(self::$modeler,'model'),array()) : forward_static_call_array(array(self::$modeler,'model'), array($_model));
         $card_object = (object) null;
         $card_object->head = 'Account Detail';
         
@@ -14,21 +15,19 @@ class SQDE_AccountCardObjects {
         $html = $js = array();
         
         $card_object->body[] = '<div class="subline kids">Username</div>';
-        $card_object->body[] = $user_model->username;
+        $card_object->body[] = $_model->username;
         $card_object->body[] = '<div class="subline kids">Role</div>';
-        SQDE_Role::exists($user_model->role_id,'id');
+        SQDE_Role::exists($_model->role_id,'id');
         $card_object->body[] = SQDE_Role::model()->name;
         $card_object->body[] = '<div class="subline kids">Sign Up Date</div>';
-        $card_object->body[] = date('g:ia \o\n l jS F Y',$user_model->sign_up_date);
+        $card_object->body[] = date('g:ia \o\n l jS F Y',$_model->sign_up_date);
         $card_object->body[] = '<div class="subline kids">Allowed Sequode Count</div>';
-        $card_object->body[] = $user_model->allowed_sequode_count;
+        $card_object->body[] = $_model->allowed_sequode_count;
         $card_object->body[] = '<div class="subline kids">Sequode Favorites</div>';
-        $card_object->body[] = $user_model->sequode_favorites;
+        $card_object->body[] = $_model->sequode_favorites;
         $card_object->body[] = '<div class="subline kids">Email</div>';
-        $card_object->body[] = $user_model->email;
-        $card_object->body[] = '<div class="subline kids">Domain</div>';
-        $card_object->body[] = $user_model->domain;
-        $sequodes_model = SQDE_UserOperations::getSequodesModelOfAllSequodes($user_model,'id,name');
+        $card_object->body[] = $_model->email;
+        $sequodes_model = SQDE_UserOperations::getSequodesModelOfAllSequodes($_model,'id,name');
         $card_object->body[] = '<div class="subline kids">Sequodes Created : '.count($sequodes_model->all).'</div>';
         $dom_id = SQDE_Component::uniqueHash();
         foreach($sequodes_model->all as $i => $object){
@@ -40,7 +39,9 @@ class SQDE_AccountCardObjects {
             $js[] = SQDE_ComponentJS::onTapEventsAjaxCall($dom_id.$i, SQDE_ComponentJS::ajaxCallObject('cards/sequode/details', array($object->id)));
             $card_object->body[] = (object) array('html' => implode('',$html).'js' => implode('',$js));
         }
-        $card_object->body[] = '<div style="position:absolute; bottom:5px; right:5px;" class="subline kids">Id: '.$user_model->id.'</div>';
+        if(SQDE_UserAuthority::isSystemOwner()){
+            $card_object->body[] = '<div style="position:absolute; bottom:5px; right:5px;" class="subline kids">Id: '.$_model->id.'</div>';
+        }
         return $card_object;
     }
 }
