e39549d78db821627039d11fca82502d7b6a111d
shawnlawyer <shawnlawyer@gmail.com>
2015-08-17 03:35:47 -0400

    moo

--- classes/Packages/Site/SQDE_SiteCardsAjax.class.php
+++ classes/Packages/Site/SQDE_SiteCardsAjax.class.php
@@ -36,25 +36,25 @@ class SQDE_SiteCardsAjax {
         $html[] = SQDE_Card::menuCardHidingContainer($card->html,9);
         $js[] = $card->js;
         if(SQDE_UserAuthority::isAuthenticated()){
-            $card = SQDE_Cards::render('Sequode','menu');
-            $html[] = SQDE_Card::menuCardHidingContainer($card->html,8);
-            $js[] = $card->js;
+            //$card = SQDE_Cards::render('Sequode','menu');
+            //$html[] = SQDE_Card::menuCardHidingContainer($card->html,8);
+            //$js[] = $card->js;
             if(SQDE_AuthenticatedUser::model()->role_id < 101){
                 //$card = SQDE_Cards::render('Users','menu');
                 //$html[] = SQDE_Card::menuCardHidingContainer($card->html,7);
                 //$js[] = $card->js;
-                $card = SQDE_Cards::render('Package','menu');
-                $html[] = SQDE_Card::menuCardHidingContainer($card->html,6);
-                $js[] = $card->js;
-                $card = SQDE_Cards::render('Machine','menu');
-                $html[] = SQDE_Card::menuCardHidingContainer($card->html,5);
-                $js[] = $card->js;
+                //$card = SQDE_Cards::render('Package','menu');
+                //$html[] = SQDE_Card::menuCardHidingContainer($card->html,6);
+               // $js[] = $card->js;
+                //$card = SQDE_Cards::render('Machine','menu');
+                //$html[] = SQDE_Card::menuCardHidingContainer($card->html,5);
+                //$js[] = $card->js;
             }
         }
         if(SQDE_UserAuthority::isSystemOwner()){ 
-            $card = SQDE_Cards::render('Session','menu');
-            $html[] = SQDE_Card::menuCardHidingContainer($card->html,4);
-            $js[] = $card->js;
+            //$card = SQDE_Cards::render('Session','menu');
+            //$html[] = SQDE_Card::menuCardHidingContainer($card->html,4);
+            //$js[] = $card->js;
         }
         return SQDE_BrowserRemote::addIntoDom($dom_id, implode('',$html), 'replace'). implode(' ',$js);
     }
