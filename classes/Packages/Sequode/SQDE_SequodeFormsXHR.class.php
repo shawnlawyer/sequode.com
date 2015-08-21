8f3e2539ced185741e7f49c15a09796bbaec6cc7
shawnlawyer <shawnlawyer@gmail.com>
2015-08-21 06:14:44 -0400

    moo

--- classes/Packages/Sequode/SQDE_SequodeFormsXHR.class.php
+++ classes/Packages/Sequode/SQDE_SequodeFormsXHR.class.php
@@ -11,15 +11,6 @@ class SQDE_SequodeFormsXHR {
         )){return;}
         return SQDE_ComponentJS::placeForm(SQDE_Forms::render(self::$package,__FUNCTION__), $dom_id);
     }
-    public static function name($_model_id, $dom_id){
-        $modeler = static::$modeler;
-        if(!(
-        $modeler::exists($_model_id,'id')
-        && (SQDE_UserAuthority::isOwner( $modeler::model() )
-        || SQDE_UserAuthority::isSystemOwner())
-        )){return;}
-        return SQDE_ComponentJS::placeForm(SQDE_Forms::render(self::$package,__FUNCTION__), $dom_id);
-    }
     public static function description($_model_id, $dom_id){
         $modeler = static::$modeler;
         if(!(
