95bb92526149e7a9095357992b8f582ee9384ae7
shawnlawyer <shawnlawyer@gmail.com>
2015-08-21 05:43:49 -0400

    moo

--- classes/Packages/Users/SQDE_UsersFormsXHR.class.php
+++ classes/Packages/Users/SQDE_UsersFormsXHR.class.php
@@ -1,9 +1,10 @@
 <?php
 class SQDE_UsersFormsXHR {
     public static $package = 'Users';
+    public static $modeler = 'SQDE_User';
     public static function updatePassword($_model_id, $dom_id){
         if(!(
-            SQDE_User::exists($_model_id,'id')
+        {static::$modeler}::exists($_model_id,'id')
         )){return;}
         return SQDE_ComponentJS::placeForm(SQDE_Forms::render(self::$package,__FUNCTION__), $dom_id);
     }
