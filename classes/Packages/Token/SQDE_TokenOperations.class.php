0881ff1d057c142e48562665e9b5fe16401f7531
shawnlawyer <shawnlawyer@gmail.com>
2015-08-23 22:15:36 -0400

    moo

--- classes/Packages/Token/SQDE_TokenOperations.class.php
+++ classes/Packages/Token/SQDE_TokenOperations.class.php
@@ -1,14 +1,14 @@
 <?php
 class SQDE_TokenOperations {
+    public static $package = 'Token';
 	//public static function uniqueHash($prefix=''){
     //    return $prefix.openssl_digest(uniqid(rand(), true).microtime(), 'sha512');
 	//}
 	public static function uniqueHash($prefix='SQDE'){
 		return $prefix.sha1(microtime().uniqid(rand(), true));
 	}
-    public static $modeler = 'SQDE_Token';
     public static function getModel($value = null, $by = null, $owner_id = null){
-        $modeler = static::$modeler;
+        $modeler = SQDE_PackagesHandler::model(static::$package)->modeler;
         $_model = new $modeler::$model;
         switch($by){
             case 'id':
@@ -37,11 +37,13 @@ class SQDE_TokenOperations {
         return false;   
 	}
     public static function updateName($name, $_model = null){
+        $modeler = SQDE_PackagesHandler::model(static::$package)->modeler;
         ($_model == null) ? forward_static_call_array(array(self::$modeler,'model'),array()) : forward_static_call_array(array(self::$modeler,'model'),array($_model));
         $modeler::model()->updateField(str_replace(" ","_",$name),'name');
         return $modeler::model();
     }
     public static function newToken($owner = 0){
+        $modeler = SQDE_PackagesHandler::model(static::$package)->modeler;
         $modeler::model()->create();
         $modeler::exists($modeler::model()->id,'id');
         $modeler::model()->updateField(substr(self::uniqueHash(),0,15),'name');
@@ -49,7 +51,9 @@ class SQDE_TokenOperations {
         return $modeler::model();
 	}
     public static function delete($_model = null){
-        ($_model == null) ? forward_static_call_array(array(self::$modeler,'model'),array()) : forward_static_call_array(array(self::$modeler::model()->delete($modeler::model()->id);
+        $modeler = SQDE_PackagesHandler::model(static::$package)->modeler;
+        ($_model == null) ? forward_static_call_array(array(self::$modeler,'model'),array()) : forward_static_call_array(array(self::$modeler,'model'),array($_model));
+        $modeler::model()->delete($modeler::model()->id);
         return $modeler::model();
     }
 }
