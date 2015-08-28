8e5b578dc9d6770af0c3738768a6c414c18df84d
shawnlawyer <shawnlawyer@gmail.com>
2015-08-27 22:23:04 -0400

    moo

--- classes/Components/SQDE_CardComponent.class.php
+++ classes/Components/SQDE_CardComponent.class.php
@@ -4,11 +4,10 @@ class SQDE_CardComponent {
         if($user_model == null ){ $user_model = SQDE_AuthenticatedUser::model(); }
         $dom_id = SQDE_Component::uniqueHash('','');
         $html = $js = array();
-        
         $context = SQDE_PackagesHandler::model($package)->context;
         $_models = SQDE_UserOperations::getOwnedModels($package, $user_model, 'id,name')->all;
-        $_o->body[] = SQDE_CardComponentHTML::sublineBlock('Sequodes Created : '.count($_model->all));
-        $html[] = SQDE_CardComponentHTML::sublineBlock($headline . count($_model->all));
+        $html[] = '<div class="automagic-card-menu-item noSelect" id="'.$dom_id.'">'.$headline . count($_model).'</div>';
+        $js[] = SQDE_ComponentJS::onTapEventsXHRCall($dom_id, SQDE_ComponentJS::xhrCallObject('cards/'.$context.'/my', array($object->id)));
         foreach($_models as $i => $object){
             $html[] = '<div class="automagic-card-menu-item noSelect" id="'.$dom_id.$i.'">';
             $html[] = $object->name;
