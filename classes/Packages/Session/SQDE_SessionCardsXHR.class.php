1208302b88f6e4067dea8b82d9ac32c611911394
shawnlawyer <shawnlawyer@gmail.com>
2015-08-28 05:21:31 -0400

    moo

--- classes/Packages/Session/SQDE_SessionCardsXHR.class.php
+++ classes/Packages/Session/SQDE_SessionCardsXHR.class.php
@@ -1,9 +1,8 @@
 <?php
 class SQDE_SessionCardsXHR {
     public static $package = 'Session';
-    public static $modeler = 'SQDE_Session';
     public static function details($_model_id=0, $dom_id = 'MagicCardsContainer'){
-        $modeler = static::$modeler;
+        $modeler = SQDE_PackagesHandler::model(static::$package)->modeler;
         if(!(
             $modeler::exists($_model_id,'id')
         )){ return; }
