9d71d310b0f983bb8e2606679593fa322dfa26c8
shawnlawyer <shawnlawyer@gmail.com>
2015-09-09 00:15:42 -0400

    too

--- classes/Modules/Register/SQDE_RegisterOperationsXHR.class.php
+++ classes/Modules/Register/SQDE_RegisterOperationsXHR.class.php
@@ -17,18 +17,22 @@ class SQDE_RegisterOperationsXHR {
         //&& SQDE_UserAuthority::isSecurePassword(rawurldecode($input->password))
         )){return;}
         $operations = SQDE_PackagesHandler::model(static::$package)->operations;
+        $cards_xhr = SQDE_PackagesHandler::model(static::$package)->xhr->cards;
         forward_static_call_array(array($operations,__FUNCTION__),array(rawurldecode($input->username),rawurldecode($input->password),rawurldecode($input->email)));
-        return 'alert(\'check email\');';
+        $js = array();
+        $js[] = forward_static_call_array(array($cards_xhr,'verify'),array());
+        $js[] = 'alert(\'check email\');';
+        return implode(' ', $js);
     }
     public static function verify($json){
         $js = array();
         $input = json_decode(rawurldecode($json));
         if(!(
-        SQDE_User::exists($input->token,'activation_token')
+        SQDE_User::exists(rawurldecode($input->token),'activation_token')
         && SQDE_User::model()->active == 0
         )){return;}
         $operations = SQDE_PackagesHandler::model(static::$package)->operations;
-        forward_static_call_array(array($operations,__FUNCTION__),array(rawurldecode($input->token)));
+        forward_static_call_array(array($operations,__FUNCTION__),array(rawurldecode(SQDE_User::model())));
         return 'alert(\'verified\');';
     }
 }
