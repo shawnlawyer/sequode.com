5114d3f36dc5853340d35e2dd499ddbb29643317
shawnlawyer <shawnlawyer@gmail.com>
2015-09-12 06:25:50 -0400

    too

--- classes/Modules/Register/SQDE_RegisterCardsXHR.class.php
+++ classes/Modules/Register/SQDE_RegisterCardsXHR.class.php
@@ -1,13 +1,14 @@
 <?php
 class SQDE_RegisterCardsXHR {
     public static $package = 'Register';
+	public static $merge = false;
+	public static $routes = array(
+		'signup'
+	);
+	public static $routes_to_methods = array(
+		'signup' => 'signup'
+    );
     public static function signup($dom_id = 'MagicCardsContainer'){
-        return SQDE_ComponentJS::placeCard(SQDE_Cards::render(self::$package,__FUNCTION__), $dom_id);
-    }
-    public static function verify($dom_id = 'MagicCardsContainer'){
-        return SQDE_ComponentJS::placeCard(SQDE_Cards::render(self::$package,__FUNCTION__), $dom_id);
-    }
-    public static function terms($dom_id = 'MagicCardsContainer'){
         return SQDE_ComponentJS::placeCard(SQDE_Cards::render(self::$package,__FUNCTION__), $dom_id);
     }
 }
--- classes/Modules/Register/SQDE_RegisterFormObjects.class.php
+++ classes/Modules/Register/SQDE_RegisterFormObjects.class.php
@@ -3,13 +3,15 @@ class SQDE_RegisterFormObjects   {
     public static $package = 'Register';
 	public static $objects_source = 'SQDE_RegisterFormComponentObjects';
 	public static $xhr_library = 'operations/register';
-    public static function signup(){
+    public static function email(){
         $_o = SQDE_Form::formObject(static::$objects_source, __FUNCTION__, static::$xhr_library, func_get_args());
+        $_o->submit_xhr_call_route = static::$xhr_library.'/'.'signup';
         $_o->submit_button = 'Submit';
 		return $_o;
 	}
     public static function verify(){
         $_o = SQDE_Form::formObject(static::$objects_source, __FUNCTION__, static::$xhr_library, func_get_args());
+        $_o->submit_xhr_call_route = static::$xhr_library.'/'.'signup';
         $_o->submit_button = 'Submit';
 		return $_o;
 	}
@@ -18,12 +20,6 @@ class SQDE_RegisterFormObjects   {
 		return $_o;
 	}
 	public static function acceptTerms(){
-        $_o = SQDE_Form::formObject(static::$objects_source, __FUNCTION__, static::$xhr_library, func_get_args());
-        $_o->submit_xhr_call_route = static::$xhr_library.'/'.'guest';
-        $_o->submit_button = 'Submit';
-		return $_o;
-	}
-    public static function email(){
         $_o = SQDE_Form::formObject(static::$objects_source, __FUNCTION__, static::$xhr_library, func_get_args());
         $_o->submit_xhr_call_route = static::$xhr_library.'/'.'signup';
         $_o->submit_button = 'Submit';
