a7afe7d4ee0e63f42a3840e857bb441a3f332392
shawnlawyer <shawnlawyer@gmail.com>
2015-09-14 01:56:12 -0400

    too

--- classes/Modules/Auth/SQDE_AuthCardsXHR.class.php
+++ classes/Modules/Auth/SQDE_AuthCardsXHR.class.php
@@ -3,5 +3,5 @@ class SQDE_AuthCardsXHR {
     public static $package = 'Auth';
     public static function login($dom_id = 'MagicCardsContainer'){
         return SQDE_ComponentJS::placeCard(SQDE_Cards::render(self::$package,__FUNCTION__), $dom_id);
-    
+    }
 }
