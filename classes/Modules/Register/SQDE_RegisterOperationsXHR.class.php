018e942a738455ecfd5cc1c04b36cbb8e5cc09fb
shawnlawyer <shawnlawyer@gmail.com>
2015-09-14 13:45:20 -0400

    too

--- classes/Modules/Register/SQDE_RegisterOperationsXHR.class.php
+++ classes/Modules/Register/SQDE_RegisterOperationsXHR.class.php
@@ -47,7 +47,7 @@ class SQDE_RegisterOperationsXHR {
         $modeler = SQDE_PackagesHandler::model(static::$package)->modeler;
         $input = json_decode(rawurldecode($json));
         if(!(
-        //!$modeler::exists(rawurldecode($input->email),'email')
+        !$modeler::exists(rawurldecode($input->email),'email')
         SQDE_UserAuthority::isAnEmailAddress(rawurldecode($input->email))
         )){return false;}
         return array(rawurldecode($input->email));
