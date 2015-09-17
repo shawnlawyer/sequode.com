a64e23bed00b12ff2a1b3a17d709a7c22ae44b3a
shawnlawyer <shawnlawyer@gmail.com>
2015-09-16 23:57:32 -0400

    too

--- classes/Components/SQDE_CardComponentHTML.class.php
+++ classes/Components/SQDE_CardComponentHTML.class.php
@@ -1,9 +1,9 @@
 <?php
 class SQDE_CardComponentHTML {
     public static function modelId(&$_model){
-        return '<div style="position:absolute; bottom:5px; right:5px;" class="subline kids">Id: '.$_model->id.'</div>';
+        return '<div style="position:absolute; bottom:5px; right:5px;" class="subline ">Id: '.$_model->id.'</div>';
     }
     public static function sublineBlock($content){
-        return '<div class="subline kids">'.$content.'</div>';
+        return '<div class="subline ">'.$content.'</div>';
     }
 }
