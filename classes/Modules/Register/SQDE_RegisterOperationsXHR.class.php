4563153cee3f4f81bca52f040ef453655a44ee3c
shawnlawyer <shawnlawyer@gmail.com>
2015-09-22 17:39:08 -0400

    too

--- classes/Modules/Register/SQDE_RegisterOperationsXHR.class.php
+++ classes/Modules/Register/SQDE_RegisterOperationsXHR.class.php
@@ -7,33 +7,37 @@ class SQDE_RegisterOperationsXHR {
         'reset'
 	);
 	public static $routes_to_methods = array(
-		'signup' => 'main',
+		'signup' => 'signupDialog',
 		'reset' => 'reset'
     );
-    public static function main(){
-        if(!SQDE_Session::is('registration_step')){return;}
-        $steps = array(
-            (object) array(
-                'prep' => 'verifyEmailAddress',
-                'operation' => 'setEmailAddress'
-            ),
-            (object) array(
-                'prep' => 'verifyPassword',
-                'operation' => 'setPassword'
-            ),
-            (object) array(
-                'prep' => 'verifyAccept',
-                'operation' => 'sendToken'
-            ),
-            (object) array(
-                'prep' => 'verifyToken',
-                'operation' => 'setActive'
-            )
+	public static $dialogs = array(
+        'signup' => (object) array(
+            'steps' => array(
+                (object) array(
+                    'prep' => 'verifyEmailAddress',
+                    'operation' => 'setEmailAddress'
+                ),
+                (object) array(
+                    'prep' => 'verifyPassword',
+                    'operation' => 'setPassword'
+                ),
+                (object) array(
+                    'prep' => 'verifyAccept',
+                    'operation' => 'sendToken'
+                ),
+                (object) array(
+                    'prep' => 'verifyToken',
+                    'operation' => 'setActive'
+                )
+            );
         );
-        $step = $steps[SQDE_Session::get('registration_step')];
+    );
+    
+    public static function signupDialog(){
+        $step = static::$dialogs[__FUNCTION__]->steps[SQDE_Session::get('registration_step')];
         $operations_xhr = SQDE_PackagesHandler::model(static::$package)->xhr->operations;
         $operations = SQDE_PackagesHandler::model(static::$package)->operations;
-        $parameters = forward_static_call_array(array($operations_xhr, $step->prep), func_get_args());
+        $parameters = forward_static_call_array(array($operations_xhr, 'signupPrep'), func_get_args());
         if(!(
             is_array($parameters)
             && forward_static_call_array(array($operations, $step->operation),$parameters)
@@ -45,40 +49,33 @@ class SQDE_RegisterOperationsXHR {
         $js[] = forward_static_call_array(array($cards_xhr,'signup'),array());
         return implode(' ', $js);  
     }
-    public static function verifyEmailAddress($json){
+    public static function signupPrep($json){
+        if(!SQDE_Session::is('registration_step')){return;}
         $modeler = SQDE_PackagesHandler::model(static::$package)->modeler;
         $input = json_decode(rawurldecode($json));
-        if(!(
-        !$modeler::exists(rawurldecode($input->email),'email')
-        && SQDE_UserAuthority::isAnEmailAddress(rawurldecode($input->email))
-        )){return false;}
-        return array(rawurldecode($input->email));
-    }
-    public static function verifyPassword($json){
-        $modeler = SQDE_PackagesHandler::model(static::$package)->modeler;
-        $input = json_decode(rawurldecode($json));
-        if(!(
-            rawurldecode($input->password) == rawurldecode($input->confirm_password)
-            && SQDE_UserAuthority::isSecurePassword(rawurldecode($input->password))
-        )){return false;}
-        return array(rawurldecode($input->password));
-    }
-    public static function verifyAccept($json){
-        $input = json_decode(rawurldecode($json));
-        if(!(
-            intval($input->accept) == 1
-        )){return false;}
-        return array();
-    }
-    public static function verifyToken($json){
-        $modeler = SQDE_PackagesHandler::model(static::$package)->modeler;
-        $input = json_decode(rawurldecode($json));
-        if(!(
-            $modeler::exists($input->token,'activation_token')
-            && $modeler::model()->activation_token == SQDE_Session::get('registration_token')
-            && $modeler::model()->active == 0
-        )){return false;}
-        return array();
+        switch(static::$dialogs[__FUNCTION__]->steps[SQDE_Session::get('registration_step')]->prep){
+            case 'verifyEmailAddress':
+                if((
+                    !$modeler::exists(rawurldecode($input->email),'email')
+                    && SQDE_UserAuthority::isAnEmailAddress(rawurldecode($input->email))
+                )){return array(rawurldecode($input->email));}
+            case 'verifyPassword':
+                if(!(
+                    rawurldecode($input->password) == rawurldecode($input->confirm_password)
+                    && SQDE_UserAuthority::isSecurePassword(rawurldecode($input->password))
+                )){return array(rawurldecode($input->password));}
+            case 'verifyAccept':
+                if(!(
+                    intval($input->accept) == 1
+                )){return array();}
+            case 'verifyToken': 
+                if(!(
+                    $modeler::exists($input->token,'activation_token')
+                    && $modeler::model()->activation_token == SQDE_Session::get('registration_token')
+                    && $modeler::model()->active == 0
+                )){return array();}
+        }
+        return false;
     }
     public static function reset(){
         SQDE_Session::set('registration_step',0);
