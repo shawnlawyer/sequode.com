3e14ebda9bdb3f9e76020e2b4b7bb4d3015a3c7b
shawnlawyer <shawnlawyer@gmail.com>
2015-09-22 21:20:03 -0400

    too

--- classes/Modules/Authed/SQDE_AuthedOperations.class.php
+++ classes/Modules/Authed/SQDE_AuthedOperations.class.php
@@ -3,6 +3,8 @@ class SQDE_AuthedOperations {
     public static $package = 'Authed';
 	public static function logout(){
 		SQDE_Session::end();
+        $modeler = SQDE_PackagesHandler::model(static::$package)->modeler;
+        $modeler::model(null);
 		SQDE_Session::start();
 		SQDE_Session::set('console','Auth');
         return;
