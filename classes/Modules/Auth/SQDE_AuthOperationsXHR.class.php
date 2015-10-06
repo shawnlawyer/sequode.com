d63f24652040c573581f22262c6911c17a87544e
shawnlawyer <shawnlawyer@gmail.com>
2015-10-05 22:14:40 -0400

    too

--- classes/Modules/Auth/SQDE_AuthOperationsXHR.class.php
+++ classes/Modules/Auth/SQDE_AuthOperationsXHR.class.php
@@ -70,7 +70,6 @@ class SQDE_AuthOperationsXHR {
         if(!isset($error)){
             $dialog_store->step++;
             SQDE_Session::set($dialog['session_store_key'], $dialog_store);
-            $js = array();
             return (intval($dialog_store->step) == 2) ? SQDE_ConsoleRoutes::js(false) : forward_static_call_array(array($cards_xhr,__FUNCTION__),array());
         }
     }
