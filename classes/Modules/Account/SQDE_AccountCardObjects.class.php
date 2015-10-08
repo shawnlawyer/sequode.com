15bcb4862e4f81e229644c38c2458bfc509b3f5e
shawnlawyer <shawnlawyer@gmail.com>
2015-10-08 19:22:13 -0400

    too

--- classes/Modules/Account/SQDE_AccountCardObjects.class.php
+++ classes/Modules/Account/SQDE_AccountCardObjects.class.php
@@ -86,4 +86,40 @@ class SQDE_AccountCardObjects {
         $_o->body[] = (object) array('js' => '$(\'.focus-input\').focus(); $(\'.focus-input\').select();');
         return $_o;    
     }
+    public static function updateEmail(){
+        $dialog = SQDE_PackagesHandler::model(static::$package)->xhr->dialogs[__FUNCTION__];
+        $dialog_store = SQDE_Session::get($dialog['session_store_key']);
+        $step = $dialog['steps'][$dialog_store->step];
+        $_o = (object) null;
+        $_o->icon_background = 'users-icon-background';
+        $_o->size = 'small';
+        if($dialog_store->step != 0){
+            $_o->menu = (object) null;
+            $_o->menu->items = array();
+            $dom_id = SQDE_Component::uniqueHash('','');
+            $_o->menu->items[] = array(
+                'css_classes'=>'automagic-card-menu-item noSelect',
+                'id'=>$dom_id,
+                'contents'=>'Start Over',
+                'js_action'=> SQDE_ComponentJS::onTapEventsXHRCall($dom_id, SQDE_ComponentJS::xhrCallObject('operations/account/updatePassword',array(SQDE_Form::jsQuotedValue('{"reset":"1"}'))))
+                );
+        }
+        $_o->head = 'Account Email';
+        $_o->body = array('');
+        if(isset($step->content)){
+            if(isset($step->content->head)){
+                $_o->body[] = '<div class="subline">'.$step->content->head.'</div>';
+            }
+            if(isset($step->content->head)){
+                $_o->body[] = $step->content->body;
+            }
+        }
+        if(isset($step->forms)){
+            foreach($step->forms as $form){
+                $_o->body = array_merge($_o->body, SQDE_Forms::render(self::$package, $form));
+            }
+        }
+        $_o->body[] = (object) array('js' => '$(\'.focus-input\').focus(); $(\'.focus-input\').select();');
+        return $_o;    
+    }
 }
