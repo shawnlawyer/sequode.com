37ac6236e25e1b4131e264b8875b01df1d927096
shawnlawyer <shawnlawyer@gmail.com>
2016-01-05 13:58:56 -0500

    moo

--- classes/Modules/Site/SQDE_SiteCardObjects.class.php
+++ classes/Modules/Site/SQDE_SiteCardObjects.class.php
@@ -10,12 +10,16 @@ class SQDE_SiteCardObjects {
         $html[] = '<div class="subline">Introduction</div>';
         
         $html[] = '<div class="card-textblock card-text">';
-        $html[] = 'Sequode Sequencer is a tool for process creation and management in PHP. In the current state, the tool allows you to arrange PHP SPL code elements into processes without having to write the actual code. The end goal is to allow any PHP library to be arranged, not just the SPL.';
+        $html[] = 'Sequode Sequencer is a tool for process creation and management in PHP. In the current state, the tool allows you to arrange PHP SPL code elements into processes without having to write the actual code.';
         $html[] = '</div>';
         
         $html[] = '<div class="subline ">Sequences of Programming Logic</div>';
         $html[] = '<div class="card-textblock card-text">';
-        $html[] = 'The tool allows you to manage the use and flow of the code in the PHP SPL without having to wirte the code but is <span class="subline ">not</span> a code replacement tool. The tool abstracts the code elements into simple visual components and allows you to create software visually by arraging the visual elements on a stage or programmatically via a REST API.';
+        $html[] = 'The tool is<span class="subline ">not</span> a code replacement tool. The tool abstracts the code elements into simple visual components and allows you to create software visually by arraging the visual elements on a stage or programmatically via a REST API.';
+        $html[] = '</div>';
+
+        $html[] = '<div class="card-textblock card-text">';
+        $html[] = 'The end goal of this project is to allow any PHP library to be arranged, not just the SPL.';
         $html[] = '</div>';
         
         $_o->body[] = (object) array('html' => implode('',$html));
