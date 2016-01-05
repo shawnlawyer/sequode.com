416219ee824b8638ca8b4530c7d0c2fefb45131f
shawnlawyer <shawnlawyer@gmail.com>
2016-01-04 19:00:34 -0500

    moo

--- classes/Modules/Site/SQDE_SiteCardObjects.class.php
+++ classes/Modules/Site/SQDE_SiteCardObjects.class.php
@@ -12,9 +12,9 @@ class SQDE_SiteCardObjects {
         
         $html[] = '<div class="card-textblock card-text">';
         $html[] = 'Sequode Sequencer is a tool for process arrangement in PHP. In the current state, It allows you to arrange PHP SPL code elements into processes without having to write the code. The end goal is allow any PHP library to be arranged, not just the SPL.';
+        $html[] = '</div>';
         $html[] = '<div class="card-textblock card-text">';
         $html[] = 'Sequode Sequencer is <b>not</b> a code replacement tool. The sequencer allows you to manage the use and flow of the code in the PHP SPL without having to handle the code.';
-        $html[] = '</div>';
         $html[] = '</div>';
         
         $_o->body[] = (object) array('html' => implode('',$html));
