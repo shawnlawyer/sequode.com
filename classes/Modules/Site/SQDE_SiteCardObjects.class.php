7fc079a86d648c79c49755948af8b98967231a3f
shawnlawyer <shawnlawyer@gmail.com>
2016-01-04 23:04:42 -0500

    moo

--- classes/Modules/Site/SQDE_SiteCardObjects.class.php
+++ classes/Modules/Site/SQDE_SiteCardObjects.class.php
@@ -7,37 +7,18 @@ class SQDE_SiteCardObjects {
         
         $html = $js = array();
         $_o->body = array();
-        
+        $html[] = '<div class="headline alignCenter">Sequode Sequencer</div>'
         $html[] = '<div class="subline ">Introduction</div>';
         
         $html[] = '<div class="card-textblock card-text">';
-        $html[] = 'Sequode Sequencer is a tool for process arrangement in PHP. In the current state, It allows you to arrange PHP SPL code elements into processes without having to write the code. The end goal is allow any PHP library to be arranged, not just the SPL.';
+        $html[] = 'Sequode Sequencer is a tool for process arrangement in PHP. In the current state, It allows you to arrange PHP SPL code elements into processes without having to write the code. The end goal is to allow any PHP library to be arranged, not just the SPL.';
         $html[] = '</div>';
+        
+        $html[] = '<div class="subline ">Sequences of Programming Logic</div>';
         $html[] = '<div class="card-textblock card-text">';
-        $html[] = 'Sequode Sequencer is <b>not</b> a code replacement tool. The sequencer allows you to manage the use and flow of the code in the PHP SPL without having to handle the code.';
+        $html[] = 'The tool allows you to manage the use and flow of the code in the PHP SPL without having to wirte the code and is not a code replacement tool. The tool  abstracts the code elements into simple visual components and allows you to create software visually by arraging the visual elements on a stage or programmatically via a REST API.';
         $html[] = '</div>';
         
-        $_o->body[] = (object) array('html' => implode('',$html));
-        return $_o;
-    }
-    
-    public static function introduction(){
-        $_o = (object) null;
-        $_o->head = 'Sequode Sequencer for PHP';
-        $_o->component_seperator = '';
-        
-        $html = $js = array();
-        $_o->body = array();
-        
-        $html[] = '<div class="subline ">Introduction</div>';
-        
-        $html[] = '<div class="card-textblock card-text">';
-        $html[] = 'Sequode Sequencer is a tool for process arrangement in PHP. It allows you to arrange PHP SPL code elements into processes without having to write the code.';
-        $html[] = '</div>';
-                
-        $html[] = '<div class="card-textblock card-text">';
-        $html[] = 'Sequode Sequencer is <b>not</b> a code replacement tool. It works over your code and give the ability to ';
-        $html[] = '</div>';
         $_o->body[] = (object) array('html' => implode('',$html));
         return $_o;
     }
