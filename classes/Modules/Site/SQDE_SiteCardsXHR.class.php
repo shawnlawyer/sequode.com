ffddbbd42e580476c6aa94ea812527e795f2f66c
shawnlawyer <shawnlawyer@gmail.com>
2016-01-06 15:54:46 -0500

    moo

--- classes/Modules/Site/SQDE_SiteCardsXHR.class.php
+++ classes/Modules/Site/SQDE_SiteCardsXHR.class.php
@@ -3,9 +3,9 @@ class SQDE_SiteCardsXHR {
     public static $package = 'Site';
     public static function sequode($dom_id = 'CardsContainer'){
         $deck = array();
+        $deck[] = SQDE_Cards::render('Site','sequode');
         $deck[] = SQDE_Cards::render('Auth','login');
         $deck[] = SQDE_Cards::render('Register','signup');
-        $deck[] = SQDE_Cards::render('Site','sequode');
         return SQDE_ComponentJS::placeDeck($deck, $dom_id);
     }
 }
