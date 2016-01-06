d3fd1073727f5d052b8537d73a7f11abc8000557
shawnlawyer <shawnlawyer@gmail.com>
2016-01-05 19:19:25 -0500

    moo

--- classes/Modules/Console/SQDE_ConsoleCardsXHR.class.php
+++ classes/Modules/Console/SQDE_ConsoleCardsXHR.class.php
@@ -6,7 +6,6 @@ class SQDE_ConsoleCardsXHR {
     }
     public static function menus($dom_id = 'MenusContainer'){
         $html = $js = array();
-        
         $packages = SQDE_PackagesHandler::models();
         $i = count($packages);
         foreach($packages as $package => $model){
@@ -21,42 +20,4 @@ class SQDE_ConsoleCardsXHR {
         }
         return SQDE_BrowserRemote::addIntoDom($dom_id, implode('',$html), 'replace'). implode(' ',$js);
     }
-	public static function operations(){
-        
-        $packages = SQDE_PackagesHandler::models();
-        foreach($packages as $package => $model){
-            if(isset($model->xhr->operations)){
-                echo '<li> /';
-                echo $model->context;
-                echo '/ </li>';
-                $routes = SQDE_Routes::routes($model->xhr->operations);
-                foreach($routes as $route){
-                    echo '<li>';
-                    echo __FUNCTION__ .'/'. $model->context .'/'. $route;
-                    echo '</li>';
-                }
-            }
-        }
-		exit;
-	}
-	public static function cards(){
-        
-        $packages = SQDE_PackagesHandler::models();
-        foreach($packages as $package => $model){
-            echo '<li> /';
-            echo $model->context;
-            echo '/ </li>';
-            if(isset($model->xhr->cards)){
-                $routes = SQDE_Routes::routes($model->xhr->cards);
-                foreach($routes as $route){
-                    echo '<li>';
-                    echo '<a href="/?card='. $model->context .'/'. $route .'">';
-                    echo $model->context .'/'. $route;
-                    echo '</a>';
-                    echo '</li>';
-                }
-            }
-        }
-		exit;
-	}
 }
