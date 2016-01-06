86c477ea9062b174ab2e803ed132cd67af26ba16
shawnlawyer <shawnlawyer@gmail.com>
2016-01-05 19:18:56 -0500

    moo

--- classes/Modules/Site/SQDE_SiteCardsXHR.class.php
+++ classes/Modules/Site/SQDE_SiteCardsXHR.class.php
@@ -8,20 +8,4 @@ class SQDE_SiteCardsXHR {
         $deck[] = SQDE_Cards::render('Site','sequode');
         return SQDE_ComponentJS::placeDeck($deck, $dom_id);
     }
-    public static function menus($dom_id = 'MenusContainer'){
-        $html = $js = array();
-        $packages = SQDE_PackagesHandler::models();
-        $i = count($packages);
-        foreach($packages as $package => $model){
-            if(isset($model->card_objects)){
-                if(in_array('menu',get_class_methods($model->card_objects))){
-                    $i--;
-					$card = SQDE_Cards::render($package,'menu');
-                    $html[] = SQDE_Card::menuCardHidingContainer($card->html,$i);
-                    $js[] = $card->js;
-				}
-            }
-        }
-        return SQDE_BrowserRemote::addIntoDom($dom_id, implode('',$html), 'replace'). implode(' ',$js);
-    }
 }
