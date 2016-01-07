b5a8756e6fca6a2ca29edd0976d5037439dd94ca
shawnlawyer <shawnlawyer@gmail.com>
2016-01-07 14:34:46 -0500

    moo

--- includes/js/SQDE_SequencerPalette.js
+++ includes/js/SQDE_SequencerPalette.js
@@ -216,7 +216,7 @@ var SQDE_SequencerPalette = function(){
             }else{
                 sequencer.sequence_layer.draw();
                 self.detectDragModelCollision(model);
-                setTimeout(sequencer.reorderFocusedGridArea,0,true);
+                setTimeout(sequencer.reorderFocusedGridArea,0);
             }
 		});
 		return model;
