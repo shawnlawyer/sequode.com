3e3888a0871f863343f60531597bc0c905949b53
shawnlawyer <shawnlawyer@gmail.com>
2016-01-10 19:18:47 -0500

    moo

--- src/Server.class.php
+++ src/Server.class.php
@@ -22,7 +22,7 @@ class Server {
         $request_pieces = self::requestUriPieces();
 		if(isset($request_pieces[0]) && trim($request_pieces[0]) == ''){
 			foreach($routes_classes as $routes_class){
-				if(in_array('index',get_class_methods($routes_class))){
+				if(in_array('index',get_class_methods('\\'.$routes_class))){
 					$parameters = array(); 
 					unset($request_pieces);
 					forward_static_call_array(array($routes_class ,'index'), $parameters);
