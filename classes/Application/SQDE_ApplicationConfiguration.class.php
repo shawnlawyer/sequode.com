f0d0e39d6f4da12fdf44aed6d632daa6b4eb2397
shawnlawyer <shawnlawyer@gmail.com>
2016-01-11 20:32:24 -0500

    moo

--- classes/Application/SQDE_ApplicationConfiguration.class.php
+++ classes/Application/SQDE_ApplicationConfiguration.class.php
@@ -2,7 +2,6 @@
 class SQDE_ApplicationConfiguration {
 	
 	public function __construct(){
-        $_o = (object) null;
         $access_control = array( 
             'api.sequode.com',
             'xhr.sequode.com',
@@ -10,7 +9,7 @@ class SQDE_ApplicationConfiguration {
             'console.sequode.com',
             'sequode.com'
         );
-        $_o->access_control = $access_control;
+        $this->access_control = $access_control;
         $sessions = json_decode('{
             "create_domain": "sequode.com",
             "domain": "sequode.com",
@@ -18,14 +17,14 @@ class SQDE_ApplicationConfiguration {
             "path": "/",
             "cookie": "sequodeUser"
         }');
-        $_o->sessions = $sessions;
+        $this->sessions = $sessions;
         $site = json_decode('{
             "email":"noreply@sequode.com",
             "domain":"sequode.com",
             "display_name":"Sequode",
             "protocol":"https://"
         }');
-        $_o->site = $site;
+        $this->site = $site;
         $system_database = json_decode('{
             "host":"sequode-'.$_SERVER['WORKFLOW_ENVIRONMENT'].'.ckrl02wpm4xk.us-west-2.rds.amazonaws.com",
             "user":"sequode",
@@ -58,16 +57,15 @@ class SQDE_ApplicationConfiguration {
             "name":"operations"
         }');
         */
-		$_o->system_database = new SQDE_MySQLDatabase($system_database->host,$system_database->user,$system_database->password,$system_database->name);
-		$_o->sequodes_database = new SQDE_MySQLDatabase($sequodes_database->host,$sequodes_database->user,$sequodes_database->password,$sequodes_database->name);
-		$_o->accounts_database = new SQDE_MySQLDatabase($accounts_database->host,$accounts_database->user,$accounts_database->password,$accounts_database->name);
-		$_o->sessions_database = new SQDE_MySQLDatabase($sessions_database->host,$sessions_database->user,$sessions_database->password,$sessions_database->name);
-		//$_o->database = new SQDE_MySQLDatabase($database->host,$database->user,$database->password,$database->name);
+		$this->system_database = new SQDE_MySQLDatabase($system_database->host,$system_database->user,$system_database->password,$system_database->name);
+		$this->sequodes_database = new SQDE_MySQLDatabase($sequodes_database->host,$sequodes_database->user,$sequodes_database->password,$sequodes_database->name);
+		$this->accounts_database = new SQDE_MySQLDatabase($accounts_database->host,$accounts_database->user,$accounts_database->password,$accounts_database->name);
+		$this->sessions_database = new SQDE_MySQLDatabase($sessions_database->host,$sessions_database->user,$sessions_database->password,$sessions_database->name);
+		//$this->database = new SQDE_MySQLDatabase($database->host,$database->user,$database->password,$database->name);
 		if(!empty($_SERVER['WINDIR'])){
-			$_o->docRoot = 'C:/wamp/www/msystem';
+			$this->docRoot = 'C:/wamp/www/msystem';
 		}else{
-			$_o->docRoot = '/opts/nginx/html';
+			$this->docRoot = '/opts/nginx/html';
 		}
-        return $_o;
     }
 }
