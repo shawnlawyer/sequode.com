812f9f9bbba2b52f1d1526069048713738e0b0d6
shawnlawyer <shawnlawyer@gmail.com>
2016-01-16 01:18:06 -0500

    moo

--- src/ModuleCard.class.php
+++ src/ModuleCard.class.php
@@ -3,6 +3,6 @@ namespace Sequode;
 
 class ModuleCard {
 	public static function render($package, $card, $parameters = null){
-		return \Sequode\Component\Card\Card::render(\Sequode\Component\Card\Card::fetchObject('\\'.\Sequode\ModuleRegistry::model($package)->card_objects, $card, ($parameters == null) ? array() : (!is_array($parameters)) ? array($parameters) : $parameters));
+		return \Sequode\Component\Card\Card::render(\Sequode\Component\Card\Card::fetchObject(\Sequode\ModuleRegistry::model($package)->card_objects, $card, ($parameters == null) ? array() : (!is_array($parameters)) ? array($parameters) : $parameters));
 	}
 }
