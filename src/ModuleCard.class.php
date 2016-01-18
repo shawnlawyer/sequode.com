4f6903f7f7f42bbf128bed9452393fc458e398c9
shawnlawyer <shawnlawyer@gmail.com>
2016-01-17 23:08:06 -0500

    moo

--- src/ModuleCard.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Sequode;
-
-class ModuleCard {
-	public static function render($package, $card, $parameters = null){
-		return \Sequode\Component\Card\Card::render(\Sequode\Component\Card\Card::fetchObject(\Sequode\ModuleRegistry::model($package)->card_objects, $card, ($parameters == null) ? array() : (!is_array($parameters)) ? array($parameters) : $parameters));
-	}
-}
--- src/XHR.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Sequode;
-
-class XHR  {
-	public static function call($route_class, $route, $parameters = null){
-        if($parameters == null){ $parameters = array();}
-		return forward_static_call_array(array($route_class, $route), $parameters);
-    }
-}
