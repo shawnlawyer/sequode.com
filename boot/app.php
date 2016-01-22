9facab2e0c61cf8ae331c4bc83d1487c99b486b3
shawnlawyer <shawnlawyer@gmail.com>
2016-01-22 00:37:15 -0500

    moo

--- boot/app.php
+++ boot/app.php
@@ -4,7 +4,8 @@ date_default_timezone_set('America/Los_Angeles');
 ob_start('ob_gzhandler');
 
 Sequode\ApplicationConfiguration::model('\\SQDE_BuilderApplicationConfiguration');
-
+echo 'here';
+exit;
 SQDE_Session::start();
 if(SQDE_Session::is('user_id')){
     SQDE_AuthOperations::load();
