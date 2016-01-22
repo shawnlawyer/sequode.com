c27229ab95490bc28f5a87b3b234097fd3bdd30d
shawnlawyer <shawnlawyer@gmail.com>
2016-01-21 23:34:06 -0500

    moo

--- classes/Application/SQDE_ApplicationConfiguration.class.php
+++ classes/Application/SQDE_ApplicationConfiguration.class.php
@@ -1,18 +1,8 @@
 <?php
 class SQDE_ApplicationConfiguration {
-	
 	public function __construct(){
-        $sessions = json_decode('{
-            "create_domain": "sequode.com",
-            "domain": "sequode.com",
-            "length": 86400,
-            "path": "/",
-            "cookie": "sequodeUser"
-        }');
-        $this->sessions = $sessions;
-       
-        if(isset(ApplicationConfiguration::model()->database)){
-            foreach(ApplicationConfiguration::model()->database as $member => $database){
+        if(isset(\Sequode\ApplicationConfiguration::model()->database)){
+            foreach(\Sequode\ApplicationConfiguration::model()->database as $member => $database){
                 $this->database->$member . '_database' = new \Sequode\Controller\Database\MySQL($database->host,$database->user,$database->password,$database->name);
             }
         }
