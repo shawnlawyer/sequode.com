78f7b6f4e7a1706400975eaf8bf59474f7ccf20b
shawnlawyer <shawnlawyer@gmail.com>
2016-01-22 00:03:37 -0500

    moo

--- classes/Application/SQDE_ApplicationConfiguration.class.php
+++ classes/Application/SQDE_ApplicationConfiguration.class.php
@@ -4,7 +4,7 @@ class SQDE_ApplicationConfiguration {
         if(isset(\Sequode\ApplicationConfiguration::model()->database)){
             $this->database = (object) null;
             foreach(\Sequode\ApplicationConfiguration::model()->database as $member => $database){
-                $add_member = $key. '_database';
+                $add_member = $member. '_database';
                 $this->$add_member = new \Sequode\Controller\Database\MySQL($database->host,$database->user,$database->password,$database->name);
             }
         }
