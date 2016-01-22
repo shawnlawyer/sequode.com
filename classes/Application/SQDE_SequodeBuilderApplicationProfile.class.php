07a2b2c761553700d8fc96ae27dc4a7867c57e74
shawnlawyer <shawnlawyer@gmail.com>
2016-01-22 01:13:13 -0500

    moo

--- classes/Application/SQDE_SequodeBuilderApplicationProfile.class.php
+++ classes/Application/SQDE_SequodeBuilderApplicationProfile.class.php
@@ -3,24 +3,24 @@
 class SQDE_SequodeBuilderApplicationProfile {
 	public static function model(){
         $modules = array();
-        $modules['\\'.'SQDE_ConsolePackage'];
-        $modules['\\'.'SQDE_SitePackage'];
+        $modules[] = '\\'.'SQDE_ConsolePackage';
+        $modules[] = '\\'.'SQDE_SitePackage';
         
         if(SQDE_UserAuthority::isAuthenticated()){
-            $modules['\\'.'SQDE_AuthedPackage'];
-            $modules['\\'.'SQDE_SequodePackage'];
-            $modules['\\'.'SQDE_AccountPackage'];
-            $modules['\\'.'SQDE_PackagePackage'];
-            $modules['\\'.'SQDE_TokenPackage'];
+            $modules[] = '\\'.'SQDE_AuthedPackage';
+            $modules[] = '\\'.'SQDE_SequodePackage';
+            $modules[] = '\\'.'SQDE_AccountPackage';
+            $modules[] = '\\'.'SQDE_PackagePackage';
+            $modules[] = '\\'.'SQDE_TokenPackage';
         }else{
-            $modules['\\'.'SQDE_AuthPackage'];
-            $modules['\\'.'SQDE_RegisterPackage'];
+            $modules[] = '\\'.'SQDE_AuthPackage';
+            $modules[] = '\\'.'SQDE_RegisterPackage';
         }
         
         if(SQDE_UserAuthority::isSystemOwner()){
-            $modules['\\'.'SQDE_SessionPackage'];
-            $modules['\\'.'SQDE_UserPackage'];
-            $modules['\\'.'SQDE_BlacklistIPPackage'];
+            $modules[] = '\\'.'SQDE_SessionPackage';
+            $modules[] = '\\'.'SQDE_UserPackage';
+            $modules[] = '\\'.'SQDE_BlacklistIPPackage';
         }
         
         foreach($modules as $module){
