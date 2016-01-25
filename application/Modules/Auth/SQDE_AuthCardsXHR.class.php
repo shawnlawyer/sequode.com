066b2325e2812692af423afd22943957ba7be0c5
shawnlawyer <shawnlawyer@gmail.com>
2016-01-24 19:00:03 -0500

    moo

--- application/Modules/Auth/SQDE_AuthCardsXHR.class.php
+++ application/Modules/Auth/SQDE_AuthCardsXHR.class.php
@@ -6,6 +6,6 @@ use Sequode\Component\Card\Kit\JS as CardKitJS;
 class SQDE_AuthCardsXHR {
     public static $package = 'Auth';
     public static function login($dom_id = 'CardsContainer'){
-        return Card\Kit\JS::placeCard(ModuleCard::render(self::$package,__FUNCTION__), $dom_id);
+        return CardKitJS::placeCard(ModuleCard::render(self::$package,__FUNCTION__), $dom_id);
     }
 }
