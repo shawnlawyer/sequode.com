7baf9d32280a653bf78aaf6d00137745dbb604ea
shawnlawyer <shawnlawyer@gmail.com>
2016-01-31 17:48:29 -0500

    moo

--- application/Modelers/SQDE_Token.class.php
+++ application/Modelers/SQDE_Token.class.php
@@ -1,5 +1,5 @@
 <?php
 
 class SQDE_Token extends Sequode\Patterns\Modeler {
-    public static $model = '\\Sequode\\Application\\Models\\ORM\\Tokens';
+    public static $model = Sequode\Application\Models\ORM\Tokens::class;
 }
