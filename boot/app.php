86f3de7ea88bf364c2a49ab535cdfe0881d357cd
shawnlawyer <shawnlawyer@gmail.com>
2016-02-07 12:21:03 -0500

    moo

--- boot/app.php
+++ boot/app.php
@@ -1,14 +1,14 @@
 <?php
+
 use Sequode\Model\Application\Configuration;
 use Sequode\Model\Application\Runtime;
 
 date_default_timezone_set('America/Los_Angeles');
 ob_start('ob_gzhandler');
 
-Configuration::model('\\ApplicationConfiguration');
+Configuration::model(ApplicationConfiguration::class);
 \Sequode\Application\Modules\Session\Modeler::start();
 if(\Sequode\Application\Modules\Session\Modeler::is('user_id')){
     \Sequode\Application\Modules\Auth\Operations::load();
 }
-
-Runtime::model('\\RuntimeModules');
+Runtime::model(RuntimeModules::class);
