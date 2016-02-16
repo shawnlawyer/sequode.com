fc37a95d07b291d1f3bb4c69dc270d0d40a57e8d
shawnlawyer <shawnlawyer@gmail.com>
2016-02-16 02:26:04 -0500

    mo

--- boot/app.php
+++ boot/app.php
@@ -3,12 +3,17 @@
 use Sequode\Model\Application\Configuration;
 use Sequode\Model\Application\Runtime;
 
+use Sequode\Application\Modules\Session\Operations as SessionOperations;
+use Sequode\Application\Modules\Session\Store as SessionStore;
+
 date_default_timezone_set('America/Los_Angeles');
 ob_start('ob_gzhandler');
 
 Configuration::model(\ApplicationConfiguration::class);
-\Sequode\Application\Modules\Session\Operations::start();
-if(\Sequode\Application\Modules\Session\Operations::is('user_id')){
+
+SessionOperations::start();
+if(SessionOperations::is('user_id')){
     \Sequode\Application\Modules\Auth\Operations::load();
 }
+
 Runtime::model(\RuntimeModules::class);
