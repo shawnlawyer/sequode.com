fcbcf2616519f9966d8fd853027aeb34213d69b2
shawnlawyer <shawnlawyer@gmail.com>
2016-03-09 15:32:25 -0500

    moo

--- boot/autoload.php
+++ boot/autoload.php
@@ -1,21 +1,21 @@
 <?php
 
-spl_autoload_register(function($class){
-    $extention = '.class.php';
-    $directories = explode((!empty($_SERVER["WINDIR"])) ? ';' : ':', get_include_path());
-    foreach($directories as $directory){
-		$file = $directory . DIRECTORY_SEPARATOR . $class . $extention;
-		if(file_exists($file)){
-			require_once($file);
-            return true;
-        }
-	}
-    return false;
-});
-
 spl_autoload_register(function ($class){
     $extention = '.class.php';
     $directory = '../configure';
+    $file = $directory . DIRECTORY_SEPARATOR . trim($class,'\\') . $extention;
+    if(file_exists($file)){
+        
+        require_once($file);
+        return true;
+    }
+    return false;
+});
+
+
+spl_autoload_register(function ($class){
+    $extention = '.class.php';
+    $directory = '../lib';
     $file = $directory . DIRECTORY_SEPARATOR . trim($class,'\\') . $extention;
     if(file_exists($file)){
         
