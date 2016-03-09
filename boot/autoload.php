2d6e32f7a3669f10f39eeab68f87e96ef8ee32fd
shawnlawyer <shawnlawyer@gmail.com>
2016-03-09 14:30:35 -0500

    moo

--- boot/autoload.php
+++ boot/autoload.php
@@ -12,6 +12,7 @@ spl_autoload_register(function($class){
 	}
     return false;
 });
+/*
 spl_autoload_register(function ($class){
     $prefix = 'Sequode';
     $extention = '.class.php';
@@ -28,5 +29,42 @@ spl_autoload_register(function ($class){
             return true;
         }
 	}
+    return false;
+});
+*/
+
+spl_autoload_register(function ($class){
+    $prefix = 'Sequode';
+    $extention = '.class.php';
+    $class_pieces = explode('\\', trim($class,'\\'));
+    if($class_pieces[0] != $prefix){
+        return;
+    }
+    array_shift($class_pieces);
+    $directory = __DIR__ . '../vendor'
+    $file = $directory . DIRECTORY_SEPARATOR . $prefix . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . implode( DIRECTORY_SEPARATOR, $class_pieces ) . $extention;
+    if(file_exists($file)){
+        require_once($file);
+        return true;
+    }
+    return false;
+});
+
+spl_autoload_register(function ($class){
+    $prefix = 'Application';
+    $extention = '.class.php';
+    $class_pieces = explode('\\', trim($class,'\\'));
+    if($class_pieces[0] != $prefix){
+        return;
+    }
+    array_shift($class_pieces);
+    $directory = __DIR__ . '../app'
+    
+    $file = $directory . DIRECTORY_SEPARATOR . $prefix . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . implode( DIRECTORY_SEPARATOR, $class_pieces ) . $extention;
+    if(file_exists($file)){
+        require_once($file);
+        return true;
+    }
+	
     return false;
 });
