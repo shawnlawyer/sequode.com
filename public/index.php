14f1ff1f521c8577895cce0b32648bd7b89bbde5
Shawn Lawyer <shawnlawyer@gmail.com>
2019-02-24 16:10:13 -0500

    working runtime using \Sequode\Sequode

--- public/index.php
+++ public/index.php
@@ -1,20 +1,13 @@
 <?php
 
-ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . realpath('../resource') . DIRECTORY_SEPARATOR);
-ob_start('ob_gzhandler');
-define("BASE_PATH", realpath('../'));
-define("RESOURCE_PATH", realpath(BASE_PATH . '/resource'));
-define("WWW_PATH", realpath(BASE_PATH . '/public'));
-require_once('../vendor/autoload.php');
-
-use Sequode\Controller\Application\HTTPRequest;
-use Symfony\Component\HttpFoundation\Request as SymfonyHTTPRequest;
 use Symfony\Component\Dotenv\Dotenv;
 
-$dotenv = (new Dotenv())->load(BASE_PATH . '/.env');
-//(new \Whoops\Run())->pushHandler(new Whoops\Handler\PrettyPageHandler())->register();
-$request = SymfonyHTTPRequest::createFromGlobals();
+use Symfony\Component\HttpFoundation\Request as SymfonyHTTPRequest;
 
+use Application\HTTPAPIRequest;
+use Whoops\Handler\PrettyPageHandler;
+
+use Sequode\Controller\Application\HTTPRequest;
 use Sequode\Model\Application\Configuration as ApplicationConfiguration;
 use Sequode\Model\Application\Runtime as ApplicationRuntime;
 use Sequode\Model\Module\Registry as ModuleRegistry;
@@ -22,8 +15,22 @@ use Sequode\Application\Modules\Session\Operations as SessionOperations;
 use Sequode\Application\Modules\Session\Store as SessionStore;
 use Sequode\Application\Modules\Auth\Operations as AuthOperations;
 
-use Application\HTTPAPIRequest;
+ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . realpath('../resource') . DIRECTORY_SEPARATOR);
+ob_start('ob_gzhandler');
+define("BASE_PATH", realpath('../'));
+define("RESOURCE_PATH", realpath(BASE_PATH . '/resource'));
+define("WWW_PATH", realpath(BASE_PATH . '/public'));
+require_once('../vendor/autoload.php');
 
+$dotenv = (new Dotenv())->load(BASE_PATH . '/.env');
+//(new \Whoops\Run())->pushHandler(new Whoops\Handler\PrettyPageHandler())->register();
+$request = SymfonyHTTPRequest::createFromGlobals();
+if ($_SERVER['HTTP_HOST'] == $_ENV['SEQUODE_RUN_DOMAIN']) {
+    //(new \Whoops\Run())->pushHandler(new Whoops\Handler\PrettyPageHandler())->register();
+    $sequode = new \Sequode\Sequode('SQDEa0b14c475d6a6ffad52060091f9a973f');
+    $sequode->http();
+    exit();
+}
 ApplicationConfiguration::model(Application\Application::class);
 if ($_SERVER['HTTP_HOST'] != $_ENV['SEQUODE_API_DOMAIN']){
     SessionOperations::start();
@@ -34,10 +41,10 @@ if ($_SERVER['HTTP_HOST'] != $_ENV['SEQUODE_API_DOMAIN']){
 }
 ModuleRegistry::model(Application\Modules::class);
 ApplicationRuntime::model(Application\Runtime::class);
-if ($_SERVER['HTTP_HOST'] != $_ENV['SEQUODE_API_DOMAIN']){
-    HTTPRequest::run();
-}else{
+if ($_SERVER['HTTP_HOST'] == $_ENV['SEQUODE_API_DOMAIN']) {
     HTTPAPIRequest::rest();
+} else {
+    HTTPRequest::run();
 }
 
 
