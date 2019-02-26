2000bb4dbcafaf1036815f6516d926b5451ec5bf
Shawn Lawyer <shawnlawyer@gmail.com>
2019-02-25 22:16:18 -0500

    moved and renamed CardsTrait to be in a more relevent location to it's use and renamed specific to it's use for card view requests in a module

--- application/Modules/ApplicationConsole/Routes/XHR/Cards.php
+++ application/Modules/ApplicationConsole/Routes/XHR/Cards.php
@@ -2,24 +2,17 @@
 
 namespace Application\Modules\ApplicationConsole\Routes\XHR;
 
-use Sequode\View\Module\Card as ModuleCard;
-
-use Sequode\Component\Card\Kit\JS as CardKitJS;
-use Sequode\Component\Card\Kit\HTML as CardKitHTML;
-use Sequode\Component\DOMElement\Kit\JS as DOMElementKitJS;
-
-use Application\Modules\ApplicationConsole\Module;
-
-use Sequode\Component\Card\Traits\CardsTrait;
+use Sequode\Application\Modules\Traits\Routes\XHR\CardsCardRouteTrait as XHRCardsCardRouteTrait;
 use Sequode\Application\Modules\Console\Traits\Routes\XHR\CardsConsoleMenusTrait;
-
 
 class Cards {
 
-    use CardsTrait, CardsConsoleMenusTrait;
+    use XHRCardsCardRouteTrait, CardsConsoleMenusTrait;
 
     public static $module = Module::class;
+
     public static $merge = true;
+
     public static $routes = [
         'index'
     ];
