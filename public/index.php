7cfc3cf18c0e4b5342c8ae19627d6242545edfc9
Shawn Lawyer <shawnlawyer@gmail.com>
2019-03-08 02:09:41 -0500

    cleaned up index.php, added env vars for console domain

--- public/index.php
+++ public/index.php
@@ -1,14 +1,6 @@
 <?php
 
-ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . realpath('../resource') . DIRECTORY_SEPARATOR);
-ob_start('ob_gzhandler');
-define("BASE_PATH", realpath('../'));
-define("RESOURCE_PATH", realpath(BASE_PATH . '/resource'));
-define("WWW_PATH", realpath(BASE_PATH . '/public'));
-require_once('../vendor/autoload.php');
 use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\HttpFoundation\Request as SymfonyHTTPRequest;
-use Whoops\Handler\PrettyPageHandler;
 use Sequode\Controller\Application\Request\Site\HTTP as SiteHTTPRequest;
 use Sequode\Controller\Application\Request\API\REST as APIRestRequest;
 use Sequode\Sequode;
@@ -18,30 +10,36 @@ use Sequode\Application\Modules\Session\Operations as SessionOperations;
 use Sequode\Application\Modules\Session\Store as SessionStore;
 use Sequode\Application\Modules\Auth\Operations as AuthOperations;
 
+ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . realpath('../resource') . DIRECTORY_SEPARATOR);
+ob_start('ob_gzhandler');
+define("BASE_PATH", realpath('../'));
+define("RESOURCE_PATH", realpath(BASE_PATH . '/resource'));
+define("WWW_PATH", realpath(BASE_PATH . '/public'));
 
+require_once('../vendor/autoload.php');
 
-$dotenv = (new Dotenv())->load(BASE_PATH . '/.env');
-//(new \Whoops\Run())->pushHandler(new Whoops\Handler\PrettyPageHandler())->register();
-$request = SymfonyHTTPRequest::createFromGlobals();
-if ($_SERVER['HTTP_HOST'] == $_ENV['SEQUODE_RUN_DOMAIN']) {
-    //(new \Whoops\Run())->pushHandler(new Whoops\Handler\PrettyPageHandler())->register();
-    $sequode = new Sequode('SQDEa0b14c475d6a6ffad52060091f9a973f');
-    $sequode->http();
-    exit();
+(new Dotenv())->load(BASE_PATH . '/.env');
+if($_ENV['WHOOPS_ENABLED']){
+    (new \Whoops\Run())->pushHandler(new Whoops\Handler\PrettyPageHandler())->register();
 }
-ApplicationConfiguration::model(Application\Configuration::class);
-if ($_SERVER['HTTP_HOST'] != $_ENV['SEQUODE_API_DOMAIN']){
-    SessionOperations::start();
-    if (SessionStore::is('owner_id'))
-    {
-        AuthOperations::load();
-    }
-}
-ModuleRegistry::model(Application\Modules::class);
-if ($_SERVER['HTTP_HOST'] == $_ENV['SEQUODE_API_DOMAIN']) {
-    APIRestRequest::rest();
-} else {
-    SiteHTTPRequest::handle();
+switch($_SERVER['HTTP_HOST']){
+    case $_ENV['SEQUODE_CONSOLE_DOMAIN']:
+        ApplicationConfiguration::model(Application\Configuration::class);
+        SessionOperations::start();
+        if (SessionStore::is('owner_id')){
+            AuthOperations::load();
+        }
+        ModuleRegistry::model(Application\Modules::class);
+        SiteHTTPRequest::handle();
+        break;
+    case $_ENV['SEQUODE_API_DOMAIN']:
+        ApplicationConfiguration::model(Application\Configuration::class);
+        APIRestRequest::handle();
+        break;
+    case $_ENV['SEQUODE_RUN_DOMAIN']:
+        (new Sequode($_ENV('SEQUODE_RUN_PACKAGE_TOKEN')))->http();
+        break;
+
 }
 
 
