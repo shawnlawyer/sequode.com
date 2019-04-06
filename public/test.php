66d6217c15b9f666a00eb659b8c6e4add2905944
Shawn Lawyer <shawnlawyer@gmail.com>
2019-04-06 01:31:40 -0400

    refactoring module variable imports

---
+++ public/test.php
@@ -0,0 +1,20 @@
+<?php
+
+class Module {
+    public static function model(){
+        return [
+            'modeler' => 'modeler',
+            'operations' => 'operations'
+        ];
+    }
+}
+
+class Operations {
+    public static $module = Module::class;
+
+    public static function test(){
+        print_r(static::$module::model());
+    }
+
+}
+Operations::test();
